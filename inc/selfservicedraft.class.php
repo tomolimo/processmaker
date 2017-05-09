f2b8eb7971fbb0083a25c05e3d9cb1afd8c65be5
tomolimo <tomolimo@gmail.com>
2017-05-09 14:00:57 +0200

    Added empty files selfservicedraft

---
+++ inc/selfservicedraft.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * selfservicedraft short summary.
+ *
+ * selfservicedraft description.
+ *
+ * @version 1.0
+ * @author morono
+ */
+class PluginProcessmakerSelfservicedraft extends CommonDBTM {
+
+}
