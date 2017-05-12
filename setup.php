b2502515a7dc45538cf9c18ccbf242103a6d7567
tomolimo <tomolimo@gmail.com>
2017-05-12 11:12:12 +0200

    Added define('GLPI_KEEP_CSRF_TOKEN', true) ; to prevent includes.php to delete the current csrf token from session Changed version to 3.1.5

--- setup.php
+++ setup.php
@@ -100,7 +100,7 @@ function plugin_version_processmaker() {
    global $LANG;
 
    return array ('name'           => 'Process Maker',
-                'version'        => '3.1.4',
+                'version'        => '3.1.5',
                 'author'         => 'Olivier Moron',
                 'homepage'       => '',
                 'minGlpiVersion' => '9.1');
