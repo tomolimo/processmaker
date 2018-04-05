9a7d11a44ab90ba36e535ff22f5d6138021e5617
p-bo <pavel.borecki@gmail.com>
2018-04-05 09:56:24 +0200

    Added Czech localization

---
+++ locales/cs_CZ.php
@@ -0,0 +1,122 @@
+<?php
+
+$LANG['processmaker']['title'][1]="ProcessMaker";
+$LANG['processmaker']['title'][2]="Proces";
+$LANG['processmaker']['title'][3]="Seznam úkolů";
+$LANG['processmaker']['title'][4]="Pověření";
+$LANG['processmaker']['title'][5]="Procesy";
+$LANG['processmaker']['title'][6]="Propojení případu";
+$LANG['processmaker']['title'][7]="Propojení případů";
+
+$LANG['processmaker']['profile']['rightmgt']="Správa oprávnění";
+$LANG['processmaker']['profile']['process_config']="Nastavení procesu";
+$LANG['processmaker']['profile']['case_delete']="Smazat případy";
+
+$LANG['processmaker']['process']['process_guid']="Nikde se neopakující identifikátor procesu";
+$LANG['processmaker']['process']['project_type_short']="Typ projektu" ;
+$LANG['processmaker']['process']['project_type']="Typ projektu (pokud není aktuální, bude změněno)" ;
+$LANG['processmaker']['process']['project_type_bpmn']='bpmn';
+$LANG['processmaker']['process']['project_type_classic']='klasické' ;
+$LANG['processmaker']['process']['hide_case_num_title_short']="Skrýt číslo a titulek případu";
+$LANG['processmaker']['process']['hide_case_num_title']="V popisech úkolů skrýt číslo a titulek";
+$LANG['processmaker']['process']['insert_task_comment_short']="Vložit kategorii úkolu";
+$LANG['processmaker']['process']['insert_task_comment']="Vložit komentáře kategorie úkolu do jeho popisu";
+$LANG['processmaker']['process']['type']="Typ požadavku (samoobslužné)";
+$LANG['processmaker']['process']['itilcategory']="ITIL kategorie (samoobslužné)";
+$LANG['processmaker']['process']['taskcategories']['guid']="Nikde se neopakující identifikátor úkolu";
+$LANG['processmaker']['process']['taskcategories']['name']="Název úkolu";
+$LANG['processmaker']['process']['taskcategories']['completename']="Úplný název";
+$LANG['processmaker']['process']['taskcategories']['start']="Spustit";
+$LANG['processmaker']['process']['taskcategories']['comment']="Komentář";
+$LANG['processmaker']['process']['taskcategories']['is_active']="Aktivní";
+$LANG['processmaker']['process']['profile']="Trvale smazat";
+
+$LANG['processmaker']['config']['name']="Název";
+$LANG['processmaker']['config']['setup']="Nastavení ProcessMaker";
+$LANG['processmaker']['config']['mysql']="Nastavení SQL serveru";
+$LANG['processmaker']['config']['URL']="URL adresa serveru (je třeba, aby byla ze stejné domény jako GLPI)";
+$LANG['processmaker']['config']['workspace']="Název pracovního prostoru";
+$LANG['processmaker']['config']['admin']['user']="Uživatelské jméno správce serveru";
+$LANG['processmaker']['config']['admin']['password']="Heslo správce serveru";
+$LANG['processmaker']['config']['theme']="Název motivu vzhledu";
+$LANG['processmaker']['config']['comments']="Komentáře";
+$LANG['processmaker']['config']['refreshprocesslist']="Synchronizovat seznam procesů";
+$LANG['processmaker']['config']['refreshtasklist']="Synchronizovat seznam úkolů";
+$LANG['processmaker']['config']['main_task_category']="Hlavní kategorie úkolu (pro změnu názvu upravte)";
+$LANG['processmaker']['config']['taskwriter']="Úkol zapsal (pro změnu jména upravte)";
+$LANG['processmaker']['config']['pm_group_name']="Skupina v ProcessMaker která bude obsahovat všechny uživatele z GLPI";
+$LANG['processmaker']['config']['maintenance']="Režim údržby" ;
+$LANG['processmaker']['config']['undermaintenance']="Probíhá údržba, zkuste to prosím později." ;
+$LANG['processmaker']['config']['empty_pm_group_name']="Aby bylo možné vybrat skupinu, je třeba nejprve zadat nastavení pro ProcessMaker!";
+$LANG['processmaker']['config']['test']="Vyzkoušet";
+$LANG['processmaker']['config']['connectionstatus']="Stav spojení";
+$LANG['processmaker']['config']['domain']="Společná doména s GLPI";
+$LANG['processmaker']['config']['domain-error']="Žádná!";
+
+$LANG['processmaker']['item']['tab']="Proces – případ";
+$LANG['processmaker']['item']['cancelledcase']="Stav: Zrušeno";
+$LANG['processmaker']['item']['pausedtask']="Stav: Úkol je pozastaven – pokračovat v něm?";
+$LANG['processmaker']['item']['completedcase']="Stav: Dokončeno";
+$LANG['processmaker']['item']['nocase']="Žádný případ pro tuto položku!";
+$LANG['processmaker']['item']['startone']="Zahájit nějaký?";
+$LANG['processmaker']['item']['selectprocess']="Vyberte proces který chcete spustit:";
+$LANG['processmaker']['item']['start']="Spustit";
+$LANG['processmaker']['item']['unpause']="Pokračovat";
+$LANG['processmaker']['item']['deletecase']="Smazat případ?" ;
+$LANG['processmaker']['item']['buttondeletecase']="Smazat" ;
+$LANG['processmaker']['item']['reassigncase']="Znovu přiřadit úkol:";
+$LANG['processmaker']['item']['buttonreassigncase']="Znovu přiřadit";
+$LANG['processmaker']['item']['cancelcase']="Zrušit případ?" ;
+$LANG['processmaker']['item']['buttoncancelcase']="Storno" ;
+$LANG['processmaker']['item']['buttondeletecaseconfirmation']="Smazat tento případ?" ;
+$LANG['processmaker']['item']['buttoncancelcaseconfirmation']="Zrušit tento případ?" ;
+
+$LANG['processmaker']['item']['case']['deleted']="Případ byl smazán!";
+$LANG['processmaker']['item']['case']['errordeleted']="Případ se nedaří smazat!";
+$LANG['processmaker']['item']['case']['cancelled']="Případ byl zrušen!";
+$LANG['processmaker']['item']['case']['errorcancelled']="Případ se nedaří zrušit!";
+$LANG['processmaker']['item']['case']['notreassigned']="Chyba při opětovném přiřazování úkolu: ";
+$LANG['processmaker']['item']['case']['assignedtoyou']="Úkol už je této osobě přiřazen!";
+$LANG['processmaker']['item']['case']['reassigned']="Úkol znovu přiřazen!";
+$LANG['processmaker']['item']['case']['casemap']="Mapa případu";
+$LANG['processmaker']['item']['case']['casehistory']='Historie případu' ;
+$LANG['processmaker']['item']['case']['dynaforms']='Dynamické formuláře';
+$LANG['processmaker']['item']['case']['changelog']='Protokol změn';
+$LANG['processmaker']['item']['case']['caseinfo']='Informace o případu';
+$LANG['processmaker']['item']['case']['viewcasemap']='Zobrazit: mapa případu';
+$LANG['processmaker']['item']['case']['viewcasehistory']='Zobrazit: historie případu';
+$LANG['processmaker']['item']['case']['viewdynaforms']='Zobrazit: dynamické formuláře';
+$LANG['processmaker']['item']['case']['viewcasechangeloghistory']='Zobrazit: záznam změn' ;
+$LANG['processmaker']['item']['case']['casechangeloghistory']='Historie protokolu změn' ;
+
+$LANG['processmaker']['item']['error'][11]="Chyba při vytváření případu!";
+$LANG['processmaker']['item']['error'][14]="Není možné vytvořit případ: nemáte k tomu oprávnění!";
+$LANG['processmaker']['item']['error'][100]= $LANG['processmaker']['item']['error'][11];
+
+$LANG['processmaker']['item']['preventsolution'][1]="Případ je v běhu!";
+$LANG['processmaker']['item']['preventsolution'][2]="Nejprve je třeba toto spravovat (viz panel „Proces – případ“)
+	!";
+
+$LANG['processmaker']['item']['task']['process']="Spojit s procesem: ";
+$LANG['processmaker']['item']['task']['case']="Titulek případu: ";
+$LANG['processmaker']['item']['task']['task']="Úkol: ";
+$LANG['processmaker']['item']['task']['comment']="##processmaker.taskcomment##" ;
+$LANG['processmaker']['item']['task']['manage']="##ticket.url##_PluginProcessmakerCase\$processmakercases" ;
+$LANG['processmaker']['item']['task']['manage_text']= "" ; //"Process - Case tab";
+
+$LANG['processmaker']['case']['statuses']['TO_DO'] = "Zpracovává se";
+$LANG['processmaker']['case']['statuses']['CANCELLED'] = "Zrušeno";
+$LANG['processmaker']['case']['statuses']['DRAFT'] = "Nové";
+$LANG['processmaker']['case']['statuses']['COMPLETED'] = "Uzavřeno";
+
+$LANG['processmaker']['search']['case']="Případ";
+$LANG['processmaker']['search']['status']="Stav";
+$LANG['processmaker']['search']['processtitle']="Titulek procesu";
+$LANG['processmaker']['search']['casetitle']="Titulek případu";
+$LANG['processmaker']['search']['hascase']="Probíhající případ?";
+
+$LANG['processmaker']['cron']['pmusers']="Synchronizuje uživatele a skupiny z GLPI do ProcessMaker."  ;
+
+$LANG['processmaker']['cron']['pmtaskactions']="Pro uplatnění akcí úkolu mezi případy."  ;
+$LANG['processmaker']['cron']['pmorphancases']['description']="Čištění osiřelých případů." ;
+$LANG['processmaker']['cron']['pmorphancases']['parameter']="Po kolik dnů ponechávat osiřelé případy";
